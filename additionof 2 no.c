6150ec04889b3202fb52e505dd9739ff45667841
Divyanshi Ojha <ojhadivyanshi11@gmail.com>
2026-09-03 20:27:52 +0530

    Add files via upload

---
+++ additionof 2 no.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  float a,b,sum;
+  printf(" Enter any two numbers\n ");
+  scanf("%f%f",&a,&b);
+  sum=a+b;
+  printf("the sum of the given digits is %f",sum);	
+}
